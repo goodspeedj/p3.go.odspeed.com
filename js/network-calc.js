c82c0e66bb90da252847852c2e87c0f59d5a662b
unknown <n0066922@LIBP03P-OKCSQUD.lm.lmig.com>
2013-11-12 16:45:44 -0500

    Added IP address/netmask validation to form.

--- js/network-calc.js
+++ js/network-calc.js
@@ -43,3 +43,32 @@ $("#num_hosts").keyup(function() {
     var value = $(this).val();
     $("#num_hosts_calc").html(value);
 });
+
+
+/**
+ * IP address validation - based on example from: 
+ * Suprotim Agarwal, "Validate IP Address using jQuery", DeVCURRY, April 27, 2010, Accessed November 12, 2013, 
+ * http://www.devcurry.com/2010/04/validate-ip-address-using-jquery.html#.Ud8U1zs3sUJ
+ */
+$(function() {
+    $.validator.addMethod('IP4Checker', function(value) {
+    var ip = "\\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\b";
+        return value.match(ip);
+    }, 'Please enter a valid IP address');
+
+    $('#netmask').validate({
+        rules: {
+    	    ip_address: {
+                required: true,
+                IP4Checker: true
+            },
+            mask: {
+            	IP4Checker: true
+            }
+        },
+        messages: {
+        	ip_address: "Please enter a valid IP address",
+        	mask: "Please enter a valid subnet mask"
+        }
+    });
+});
